b20c0ffb35d249bddd9ecf18ba7233da7cd5ab73
fahmaoui zainab <fahmaouizainab@gmail.com>
2023-12-30 12:30:42 +0100

    V1

---
+++ src/main/java/com/example/controle51/Controle51Application.java
@@ -0,0 +1,13 @@
+package com.example.controle51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controle51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Controle51Application.class, args);
+    }
+
+}
